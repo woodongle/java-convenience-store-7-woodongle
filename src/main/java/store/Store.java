bfdf7c5d1dc392391f687609d65431cd32cca7b7
woodongle <315ehdgur@gmail.com>
2024-11-08 21:18:43 +0900

    feat: 클래스 추가, products.md 파일에서 값을 가져와 저장하는 로직 추가
    1. 편의점을 관리하는 클래스 추가
      - Store
    2. 편의점에서 판매하는 상품의 정보를 관리하는 클래스 추가
      - Products
    3. products.md 파일에서 상품의 정보를 가져와 저장하는 로직 추가

---
+++ src/main/java/store/Store.java
@@ -0,0 +1,45 @@
+package store;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import store.value.Products;
+
+public class Store {
+
+    private final List<Products> products = new ArrayList<>();
+
+    public void open() {
+        try {
+            loadProducts();
+        } catch (IllegalArgumentException | IOException e) {
+            e.getMessage();
+        }
+    }
+
+    // products.md 파일에 있는 상품 정보를 가져오는 메서드
+    private void loadProducts() throws IOException {
+        FileReader fileReader = new FileReader("src/main/resources/products.md");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String string = "";
+
+        while ((string = bufferedReader.readLine()) != null) {
+            String[] product = string.split(",");
+            saveProducts(product);
+        }
+    }
+
+    // products.md 파일에서 가져온 상품을 저장하는 메서드
+    public void saveProducts(String[] product) {
+
+        // 첫 줄은 제외하고 저장하기 위해 인덱스 0번째 값이 "name"이 아닌 배열만 사용
+        if (!product[0].equals("name")) {
+            System.out.println(Arrays.toString(product));
+            products.add(new Products(product[0], Integer.parseInt(product[1])
+                    , Integer.parseInt(product[2]), product[3]));
+        }
+    }
+}
---
+++ src/main/java/store/value/Products.java
@@ -0,0 +1,22 @@
+package store.value;
+
+public class Products {
+    private String name;
+    private int price;
+    private int quantity;
+    private String promotion;
+
+    public Products(String name, int price, int quantity, String promotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = confirmNullPromotion(promotion);
+    }
+
+    private String confirmNullPromotion(String promotion) {
+        if (promotion.equals("null")) {
+            promotion = "";
+        }
+        return promotion;
+    }
+}
